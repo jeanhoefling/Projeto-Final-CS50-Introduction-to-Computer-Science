1f1eca3e00c016e67138b958d1780251ce985e0d
Jean PC <jeanalemao2006@gmail.com>
2026-05-27 10:25:37 -0300

    Dados da página pedidos sendo inseridos no db corretamente, ainda carece de validação

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ app = Flask(__name__)
 conn = sqlite3.connect("churros.db", check_same_thread=False)
 db = conn.cursor()
 
+db.execute('''CREATE TABLE IF NOT EXISTS pedidos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            nome TEXT NOT NULL, 
+            whatsapp TEXT, 
+            tradicional INTEGER,
+            recheado INTEGER,
+            mini INTEGER,
+            valor_total INTEGER,
+            observacao TEXT,
+            status TEXT)
+           ''')
+
+conn.commit()
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -19,7 +33,35 @@ def vendas():
 @app.route("/pedidos", methods=["GET", "POST"])
 def pedidos():
     if request.method == "POST":
-        return render_template("pedidos.html")
+        nome = request.form["nome"]
+        whatsapp = request.form["whatsapp"]
+        observacao = request.form["observacao"]
+
+        produtos = {}
+        produtos_lista = request.form.getlist("produtos[]")
+        quantidades_lista = request.form.getlist("quantidades[]")
+        for i in range (len(produtos_lista)):
+            produtos[produtos_lista[i]] = int(quantidades_lista[i])
+
+        valor_total = produtos.get("tradicional", 0) * 8 + produtos.get("recheado", 0) * 10 + produtos.get("mini", 0) * 5
+
+        db.execute('''INSERT INTO pedidos 
+                   (nome, whatsapp, tradicional, recheado, mini, valor_total, observacao, status) VALUES
+                   (?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (
+                        nome,
+                        whatsapp,
+                        produtos.get("tradicional", 0),
+                        produtos.get("recheado", 0),
+                        produtos.get("mini", 0),
+                        valor_total,
+                        observacao,
+                        "Em andamento"
+                        )
+                    )
+        conn.commit()
+
+        return redirect("/pedidos")
     else:
         return render_template("pedidos.html")
     
